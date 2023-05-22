6c3a6a6025fe8c53851a2ed6e92c1bd44fdaca78
vincent <vincentcharlebois@gmail.com>
2023-05-21 20:09:00 -0400

    plus de caméras

--- cameras.js
+++ cameras.js
@@ -7,7 +7,15 @@ const cameras = [
     ["Commune & McGill", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN212.jpeg"],
     ["Fleury & Papineau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN170.jpeg"],
     ["Cremazie & Papineau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN42.jpeg"],
-]
-
+    ["Cote-des-neiges & Jean-talon", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN77.jpeg"],
+    ["Christophe-Colomb & St-Zotique", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN12.jpeg"],
+    ["Hutchison, Jean-Talon & Parc", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN44.jpeg"],
+    ["St-Jean-Baptiste & Sherbrooke", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN86.jpeg"],
+    ["Acadie & Beaumont", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN100.jpeg"],
+    ["Decarie & Jean-Talon", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN214.jpeg"],
+    ["St-Donat & Sherbrooke", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN4.jpeg"],
+    ["Notre-dame & PIE-IX", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN31.jpeg"],
+    ["Frontenac & Notre-Dame", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN70.jpeg"],
+];
 
 exports.cameras = cameras;
