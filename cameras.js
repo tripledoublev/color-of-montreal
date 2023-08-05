7794fad7003735211f8e55f21f087292cc24515c
tripledoublev <vincentcharlebois@gmail.com>
2023-08-05 13:22:20 -0400

    update camera list

--- cameras.js
+++ cameras.js
@@ -1,9 +1,8 @@
 const cameras = [
-    ["Robert & Viau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN469.jpeg"],
     ["PIE-IX & Villeray", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN544.jpeg"],
     ["Beaubien & Galeries-D'anjou", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN560.jpeg"],
     ["Bellechasse & PIE-IX", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN551.jpeg"],
-    ["Notre-dame & Papineau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN225.jpeg"],
+    ["Gouin & Laurentien", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN205.jpeg"],
     ["Commune & McGill", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN212.jpeg"],
     ["Fleury & Papineau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN170.jpeg"],
     ["Cremazie & Papineau", "https://ville.montreal.qc.ca/Circulation-Cameras/GEN42.jpeg"],
